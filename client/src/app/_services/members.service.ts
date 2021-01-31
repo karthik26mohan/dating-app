aa2d579e39414bae580d1c1ba477874e88955ac9
Karthik Mohan <>
2021-01-30 19:16:13 -0600

    like - pagination

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -90,8 +90,10 @@ export class MembersService {
     return this.http.post(this.baseUrl + 'likes/' + username, {})
   }
 
-  getLikes(predicate: string) {
-    return this.http.get(this.baseUrl + 'likes?=' + predicate);
+  getLikes(predicate: string, pageNumber, pageSize) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('predicate', predicate);
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes', params);
   }
 
   deletePhoto(photoId: number) {
